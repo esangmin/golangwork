a6386fd4255a1b399ec0299d2cdfcea6d56ef9ec
SangMinLee <sang.min.lee40@gmail.com>
2019-11-11 17:44:51 +0900

    modified twitter

--- socialpoll/twittervotes/twitter.go
+++ socialpoll/twittervotes/twitter.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -89,4 +91,76 @@ func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
 	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
 	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
 	return httpClient.Do(req)
+}
+
+type tweet struct {
+	Text string
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOptions()
+	if err != nil {
+		log.Println("failed to load options:", err)
+		return
+	}
+
+	u, err := url.Parse("https://stream.twitter.com/com/1.1/statuses/filter.json")
+	if err != nil {
+		log.Println("creating filter request failed:", err)
+		return
+	}
+	query := make(url.Values)
+	query.Set("track", strings.Join(options, ","))
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(query.Encode()))
+	if err != nil {
+		log.Println("creating filter request failed:", err)
+		return
+	}
+	resp, err := makeRequest(req, query)
+	if err != nil {
+		log.Println("making request failed:", err)
+		return
+	}
+	reader := resp.Body
+	decoder := json.NewDecoder(reader)
+	for {
+		var t tweet
+		if err := decoder.Decode(&t); err != nil {
+			break
+		}
+		for _, option := range options {
+			if strings.Contains(
+				strings.ToLower(t.Text),
+				strings.ToLower(option),
+			) {
+				log.Println("vote:", option)
+				votes <- option
+			}
+		}
+	}
+}
+
+func startTwitterStream(stopchan <-chan struct{}, votes chan<- string) <-chan struct{} {
+
+	stoppedchan := make(chan struct{}, 1)
+
+	go func() {
+		defer func() {
+			stoppedchan <- struct{}{}
+		}()
+
+		for {
+			select {
+			case <-stopchan:
+				log.Println("stopping Twitter...")
+				return
+			default:
+				log.Println("Querying Twiiter...")
+				readFromTwitter(votes)
+				log.Println(" (waiting")
+				time.Sleep(10 * time.Second) // 재연결 전 대기
+			}
+		}
+	}()
+	return stoppedchan
 }
